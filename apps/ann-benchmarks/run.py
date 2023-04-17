117d1dbcce129aa35384fa804af60afabcec4b09
etiennedi <etienne@dilocker.de>
2023-04-17 09:49:27 +0200

    [skip ci] add ability to override m

--- apps/ann-benchmarks/run.py
+++ apps/ann-benchmarks/run.py
@@ -11,8 +11,8 @@ from weaviate_query import query
 from weaviategrpc import weaviate_pb2_grpc, weaviate_pb2
 
 values = {
-    "m": [4, 8, 16, 24, 32, 48, 64],
-    "shards": [1, 2, 3, 4],
+    "m": [8, 16, 24, 32, 48, 64],
+    "shards": [1],
     "efC": 512,
     "ef": [16, 24, 32, 48, 64, 96, 128, 256, 512],
 }
@@ -26,6 +26,7 @@ stub = weaviate_pb2_grpc.WeaviateStub(channel)
 
 parser.add_argument("-v", "--vectors")
 parser.add_argument("-d", "--distance")
+parser.add_argument("-m", "--max-connections")
 args = parser.parse_args()
 
 
@@ -36,6 +37,9 @@ if (args.vectors) == None:
 if (args.distance) == None:
     logger.error(f"need -d or --distance flag to point to dataset")
     sys.exit(1)
+
+if (args.max_connections) != None:
+    values["m"] = [int(x) for x in args.max_connections.split(",")]
 
 f = h5py.File(args.vectors)
 vectors = f["train"]
