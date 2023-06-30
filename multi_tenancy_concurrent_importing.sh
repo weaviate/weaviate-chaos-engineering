36768c1ec26619d6d86621659956e4ce32953a38
etiennedi <etienne@dilocker.de>
2023-06-30 13:16:09 +0200

    add concurrent multi tenancy pipeline
    should fail with schema corruption

---
+++ multi_tenancy_concurrent_importing.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+function wait_weaviate() {
+  echo "Wait for Weaviate to be ready"
+  for _ in {1..120}; do
+    if curl -sf -o /dev/null localhost:$1; then
+      echo "Weaviate is ready"
+      break
+    fi
+
+    echo "Weaviate is not ready on $1, trying again in 1s"
+    sleep 1
+  done
+}
+
+echo "Building all required containers"
+( cd apps/multi-node-references && docker build -t ref-importer . )
+
+
+# We are reusing the replication docker-compose for this, but there is nothing
+# special about the infra, it's essentially just a 3-node cluster which is
+# perfect for this test
+echo "Starting Weaviate..."
+docker-compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-1
+wait_weaviate 8080
+docker-compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-2
+wait_weaviate 8081
+docker-compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-3
+wait_weaviate 8082
+
+echo "Run importer scripts in the background"
+docker-compose -f apps/multi-tenancy-concurrent-imports/docker-compose-importers.yml up schema-resetter
+docker-compose -f apps/multi-tenancy-concurrent-imports/docker-compose-importers.yml up -d importer-01 importer-02 importer-03 importer-04
+
+echo "Run checker script in the foreground"
+docker-compose -f apps/multi-tenancy-concurrent-imports/docker-compose-importers.yml up corruption-checker --exit-code-from corruption-checker
