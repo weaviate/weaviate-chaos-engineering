b965b70a66dbfa888190d447b9e8ecddb9bf43a6
etiennedi <etienne@dilocker.de>
2023-06-30 13:26:41 +0200

    use plugin-style docker compose syntax

--- multi_tenancy_concurrent_importing.sh
+++ multi_tenancy_concurrent_importing.sh
@@ -21,20 +21,20 @@ echo "Building all required containers"
 ( cd apps/multi-node-references && docker build -t ref-importer . )
 
 
-# We are reusing the replication docker-compose for this, but there is nothing
+# We are reusing the replication docker compose for this, but there is nothing
 # special about the infra, it's essentially just a 3-node cluster which is
 # perfect for this test
 echo "Starting Weaviate..."
-docker-compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-1
+docker compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-1
 wait_weaviate 8080
-docker-compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-2
+docker compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-2
 wait_weaviate 8081
-docker-compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-3
+docker compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-3
 wait_weaviate 8082
 
 echo "Run importer scripts in the background"
-docker-compose -f apps/multi-tenancy-concurrent-imports/docker-compose-importers.yml up schema-resetter
-docker-compose -f apps/multi-tenancy-concurrent-imports/docker-compose-importers.yml up -d importer-01 importer-02 importer-03 importer-04
+docker compose -f apps/multi-tenancy-concurrent-imports/docker-compose-importers.yml up schema-resetter
+docker compose -f apps/multi-tenancy-concurrent-imports/docker-compose-importers.yml up -d importer-01 importer-02 importer-03 importer-04
 
 echo "Run checker script in the foreground"
-docker-compose -f apps/multi-tenancy-concurrent-imports/docker-compose-importers.yml up corruption-checker --exit-code-from corruption-checker
+docker compose -f apps/multi-tenancy-concurrent-imports/docker-compose-importers.yml up corruption-checker --exit-code-from corruption-checker
