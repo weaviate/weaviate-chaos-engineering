a099408d89f41890ac9b6e8cba31d1bc4e8eb551
Andrzej Liszka <andrzej.liszka@gmail.com>
2024-01-31 16:14:37 +0100

    ingestion benchmark (no geo, no objects)

--- ann_benchmark.sh
+++ ann_benchmark.sh
@@ -22,10 +22,10 @@ function wait_weaviate() {
 echo "Building all required containers"
 ( cd apps/ann-benchmarks/ && docker build -t ann_benchmarks . )
 
-echo "Starting Weaviate..."
-docker compose -f apps/weaviate-no-restart-on-crash/docker-compose.yml up -d
+# echo "Starting Weaviate..."
+# docker compose -f apps/weaviate-no-restart-on-crash/docker-compose.yml up -d
 
-wait_weaviate
+# wait_weaviate
 
 echo "Run benchmark script"
 mkdir -p datasets
@@ -42,22 +42,22 @@ mkdir -p datasets
 )
 docker run --network host -t -v "$PWD/results:/workdir/results" -v "$PWD/datasets:/datasets" ann_benchmarks python3 run.py -v /datasets/${dataset}.hdf5 -d $distance -m 32 --labels "pq=false,after_restart=false,weaviate_version=$WEAVIATE_VERSION,cloud_provider=$CLOUD_PROVIDER,machine_type=$MACHINE_TYPE,os=$OS"
 
-echo "Initial run complete, now restart Weaviate"
+# echo "Initial run complete, now restart Weaviate"
 
-docker compose -f apps/weaviate-no-restart-on-crash/docker-compose.yml stop weaviate
-docker compose -f apps/weaviate-no-restart-on-crash/docker-compose.yml start weaviate
+# docker compose -f apps/weaviate-no-restart-on-crash/docker-compose.yml stop weaviate
+# docker compose -f apps/weaviate-no-restart-on-crash/docker-compose.yml start weaviate
 
-wait_weaviate
-echo "Weaviate ready, wait 30s for caches to be hot"
-sleep 30
+# wait_weaviate
+# echo "Weaviate ready, wait 30s for caches to be hot"
+# sleep 30
 
-echo "Second run (query only)"
-docker run --network host -t -v "$PWD/results:/workdir/results" -v "$PWD/datasets:/datasets" ann_benchmarks python3 run.py -v /datasets/${dataset}.hdf5 -d $distance -m 32 --query-only --labels "pq=false,after_restart=true,weaviate_version=$WEAVIATE_VERSION,cloud_provider=$CLOUD_PROVIDER,machine_type=$MACHINE_TYPE,os=$OS"
+# echo "Second run (query only)"
+# docker run --network host -t -v "$PWD/results:/workdir/results" -v "$PWD/datasets:/datasets" ann_benchmarks python3 run.py -v /datasets/${dataset}.hdf5 -d $distance -m 32 --query-only --labels "pq=false,after_restart=true,weaviate_version=$WEAVIATE_VERSION,cloud_provider=$CLOUD_PROVIDER,machine_type=$MACHINE_TYPE,os=$OS"
 
-docker run --network host -t -v "$PWD/datasets:/datasets" \
-  -v "$PWD/results:/workdir/results" \
-  -e "REQUIRED_RECALL=$REQUIRED_RECALL" \
-  ann_benchmarks python3 analyze.py
+# docker run --network host -t -v "$PWD/datasets:/datasets" \
+#   -v "$PWD/results:/workdir/results" \
+#   -e "REQUIRED_RECALL=$REQUIRED_RECALL" \
+#   ann_benchmarks python3 analyze.py
 
 
 echo "Passed!"
--- apps/ann-benchmarks/weaviate_import.py
+++ apps/ann-benchmarks/weaviate_import.py
@@ -1,12 +1,11 @@
-import os
-import random
 from loguru import logger
-from typing import Optional
 import uuid
 import weaviate
 import weaviate.classes.config as wvc
-import h5py
 import time
+from datetime import datetime
+from typing import Dict
+from weaviate.collections.classes.types import WeaviateField
 
 class_name = "Vector"
 
@@ -24,20 +23,163 @@ def reset_schema(client: weaviate.WeaviateClient, efC, m, shards, distance):
         ),
         properties=[
             wvc.Property(
-                name="i",
+                name="int",
                 data_type=wvc.DataType.INT,
-            )
+            ),
+            wvc.Property(
+                name="number",
+                data_type=wvc.DataType.NUMBER,
+            ),
+            wvc.Property(
+                name="text",
+                data_type=wvc.DataType.TEXT,
+            ),
+            wvc.Property(
+                name="boolean",
+                data_type=wvc.DataType.BOOL,
+            ),
+            wvc.Property(
+                name="uuid",
+                data_type=wvc.DataType.UUID,
+            ),
+            wvc.Property(
+                name="date",
+                data_type=wvc.DataType.DATE,
+            ),
+            wvc.Property(
+                name="ints",
+                data_type=wvc.DataType.INT_ARRAY,
+            ),
+            wvc.Property(
+                name="numbers",
+                data_type=wvc.DataType.NUMBER_ARRAY,
+            ),
+            wvc.Property(
+                name="texts",
+                data_type=wvc.DataType.TEXT_ARRAY,
+            ),
+            wvc.Property(
+                name="booleans",
+                data_type=wvc.DataType.BOOL_ARRAY,
+            ),
+            wvc.Property(
+                name="uuids",
+                data_type=wvc.DataType.UUID_ARRAY,
+            ),
+            wvc.Property(
+                name="dates",
+                data_type=wvc.DataType.DATE_ARRAY,
+            ),
+            wvc.Property(
+                name="phone",
+                data_type=wvc.DataType.PHONE_NUMBER,
+            ),
+            # wvc.Property(
+            #     name="geo",
+            #     data_type=wvc.DataType.GEO_COORDINATES,
+            # ),
+            # wvc.Property(
+            #     name="object",
+            #     data_type=wvc.DataType.OBJECT,
+            #     nested_properties=[
+            #         wvc.Property(
+            #             name="n_int",
+            #             data_type=wvc.DataType.INT,
+            #         ),
+            #         wvc.Property(
+            #             name="n_number",
+            #             data_type=wvc.DataType.NUMBER,
+            #         ),
+            #         wvc.Property(
+            #             name="n_text",
+            #             data_type=wvc.DataType.TEXT,
+            #         ),
+            #         wvc.Property(
+            #             name="n_boolean",
+            #             data_type=wvc.DataType.BOOL,
+            #         ),
+            #         wvc.Property(
+            #             name="n_uuid",
+            #             data_type=wvc.DataType.UUID,
+            #         ),
+            #         wvc.Property(
+            #             name="n_date",
+            #             data_type=wvc.DataType.DATE,
+            #         ),
+            #         wvc.Property(
+            #             name="n_object",
+            #             data_type=wvc.DataType.OBJECT,
+            #             nested_properties=[
+            #                 wvc.Property(
+            #                     name="nn_int",
+            #                     data_type=wvc.DataType.INT,
+            #                 ),
+            #             ],
+            #         ),
+            #     ],
+            # ),
+            # wvc.Property(
+            #     name="objects",
+            #     data_type=wvc.DataType.OBJECT_ARRAY,
+            #     nested_properties=[
+            #         wvc.Property(
+            #             name="n_ints",
+            #             data_type=wvc.DataType.INT_ARRAY,
+            #         ),
+            #         wvc.Property(
+            #             name="n_numbers",
+            #             data_type=wvc.DataType.NUMBER_ARRAY,
+            #         ),
+            #         wvc.Property(
+            #             name="n_texts",
+            #             data_type=wvc.DataType.TEXT_ARRAY,
+            #         ),
+            #         wvc.Property(
+            #             name="n_booleans",
+            #             data_type=wvc.DataType.BOOL_ARRAY,
+            #         ),
+            #         wvc.Property(
+            #             name="n_uuids",
+            #             data_type=wvc.DataType.UUID_ARRAY,
+            #         ),
+            #         wvc.Property(
+            #             name="n_dates",
+            #             data_type=wvc.DataType.DATE_ARRAY,
+            #         ),
+            #         wvc.Property(
+            #             name="n_objects",
+            #             data_type=wvc.DataType.OBJECT_ARRAY,
+            #             nested_properties=[
+            #                 wvc.Property(
+            #                     name="nn_ints",
+            #                     data_type=wvc.DataType.INT_ARRAY,
+            #                 ),
+            #             ],
+            #         ),
+            #     ],
+            # ),
         ],
         inverted_index_config=wvc.Configure.inverted_index(index_timestamps=False),
         sharding_config=wvc.Configure.sharding(desired_count=shards),
     )
 
 
-def load_records(client: weaviate.WeaviateClient, vectors, compression, dim_to_seg_ratio, override):
+def load_records(
+    client: weaviate.WeaviateClient,
+    vectors,
+    compression,
+    dim_to_seg_ratio,
+    override,
+    max_records,
+    alt_data_object,
+):
     collection = client.collections.get(class_name)
     i = 0
     if vectors == None:
         vectors = [None] * 10_000_000
+    if max_records > 0 and len(vectors) > max_records:
+        vectors = vectors[:max_records]
+
     batch_size = 1000
     len_objects = len(vectors)
 
@@ -50,11 +192,12 @@ def load_records(client: weaviate.WeaviateClient, vectors, compression, dim_to_s
             if i % 10000 == 0:
                 logger.info(f"writing record {i}/{len_objects}")
 
-            data_object = {
-                "i": i,
-            }
+            base = i
+            if alt_data_object:
+                base = i + 1
+
             batch.add_object(
-                properties=data_object,
+                properties=create_data_object(base, i),
                 vector=vector,
                 collection=class_name,
                 uuid=uuid.UUID(int=i),
@@ -82,12 +225,8 @@ def load_records(client: weaviate.WeaviateClient, vectors, compression, dim_to_s
                 if i % 10000 == 0:
                     logger.info(f"writing record {i}/{len_objects}")
 
-                data_object = {
-                    "i": i,
-                }
-
                 batch.add_object(
-                    properties=data_object,
+                    properties=create_data_object(i, i),
                     vector=vector,
                     collection=class_name,
                     uuid=uuid.UUID(int=i),
@@ -98,6 +237,70 @@ def load_records(client: weaviate.WeaviateClient, vectors, compression, dim_to_s
             logger.error(err.message)
 
     logger.info(f"Finished writing {len_objects} records")
+
+
+def create_data_object(base: int, baseGeo: int) -> Dict[str, WeaviateField]:
+    return {
+        "int": base,
+        "number": base + 0.5,
+        "text": f"text{base}",
+        "boolean": base % 2 == 0,
+        "uuid": uuid.UUID(int=1234567890 + base),
+        "date": datetime.fromtimestamp(1704063600 + base).astimezone().isoformat(),
+        "ints": [base, base + 1, base + 2],
+        "numbers": [base + 0.5, base + 1.5, base + 2.5],
+        "texts": [f"text{base}", f"text{base+1}", f"text{base+2}"],
+        "booleans": [base % 2 == 0, base % 2 != 0],
+        "uuids": [
+            uuid.UUID(int=9876543210 + base),
+            uuid.UUID(int=9876543211 + base),
+            uuid.UUID(int=9876543212 + base),
+        ],
+        "dates": [
+            datetime.fromtimestamp(1735686000 + base).astimezone().isoformat(),
+            datetime.fromtimestamp(1735686001 + base).astimezone().isoformat(),
+            datetime.fromtimestamp(1735686002 + base).astimezone().isoformat(),
+        ],
+        "phone": {"defaultCountry": "pl", "input": f"{100_000_000+base}"},
+        # "geo": {
+        #     "latitude": (baseGeo % 18_000) / 100 - 90,
+        #     "longitude": (baseGeo % 36_000) / 100 - 180,
+        # },
+        # "object": {
+        #     "n_int": base,
+        #     "n_number": base + 0.5,
+        #     "n_text": f"text{base}",
+        #     "n_boolean": base % 2 == 0,
+        #     "n_uuid": uuid.UUID(int=1234567890 + base),
+        #     "n_date": datetime.fromtimestamp(1704063600 + base).astimezone().isoformat(),
+        #     "n_object": {
+        #         "nn_int": base,
+        #     },
+        # },
+        # "objects": [
+        #     {
+        #         "n_ints": [base, base + 1, base + 2],
+        #         "n_numbers": [base + 0.5, base + 1.5, base + 2.5],
+        #         "n_texts": [f"text{base}", f"text{base+1}", f"text{base+2}"],
+        #         "n_booleans": [base % 2 == 0, base % 2 != 0],
+        #         "n_uuids": [
+        #             uuid.UUID(int=9876543210 + base),
+        #             uuid.UUID(int=9876543211 + base),
+        #             uuid.UUID(int=9876543212 + base),
+        #         ],
+        #         "n_dates": [
+        #             datetime.fromtimestamp(1735686000 + base).astimezone().isoformat(),
+        #             datetime.fromtimestamp(1735686001 + base).astimezone().isoformat(),
+        #             datetime.fromtimestamp(1735686002 + base).astimezone().isoformat(),
+        #         ],
+        #         "n_objects": [
+        #             {
+        #                 "nn_ints": [base, base + 1, base + 2],
+        #             }
+        #         ],
+        #     },
+        # ],
+    }
 
 
 def wait_for_all_shards_ready(collection: weaviate.collections.Collection):
