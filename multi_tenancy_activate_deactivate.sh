5979483b68f9e31fde4decbe6cedded919dd16a5
Marcin Antas <antas.marcin@gmail.com>
2024-06-07 19:07:42 +0200

    Add support for K8s 3-node cluster

--- multi_tenancy_activate_deactivate.sh
+++ multi_tenancy_activate_deactivate.sh
@@ -4,39 +4,6 @@ set -e
 
 git submodule update --init --remote --recursive
 
-function wait_weaviate() {
-  echo "Wait for Weaviate to be ready"
-  for _ in {1..120}; do
-    if curl -sf -o /dev/null localhost:$1/v1/.well-known/ready; then
-      echo "Weaviate is ready"
-      return 0
-    fi
-
-    echo "Weaviate is not ready on $1, trying again in 1s"
-    sleep 1
-  done
-  echo "ERROR: Weaviate is not ready in port ${1} after 120s"
-  exit 1
-}
-
-function logs() {
-  docker compose -f apps/weaviate/docker-compose-replication.yml logs --tail=100000
-}
-
-function shutdown() {
-  echo "Cleaning up ressources..."
-  docker-compose -f apps/weaviate/docker-compose-replication.yml down --remove-orphans
-  rm -rf apps/weaviate/data* || true
-  docker container rm -f multi-tenancy-activate-deactivate &>/dev/null && echo 'Deleted container multi-tenancy-activate-deactivate'
-}
-trap 'logs; shutdown; exit 1' SIGINT ERR
-
-echo "Starting Weaviate..."
-docker compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-1 weaviate-node-2 weaviate-node-3
-wait_weaviate 8080
-wait_weaviate 8081
-wait_weaviate 8082
-
 echo "Building all required containers"
 ( cd apps/multi-tenancy-activate-deactivate/ && docker build -t multi-tenancy-activate-deactivate . )
 
@@ -44,4 +11,3 @@ echo "Run script"
 docker run --network host --name multi-tenancy-activate-deactivate -t multi-tenancy-activate-deactivate
 
 echo "Success"
-shutdown
