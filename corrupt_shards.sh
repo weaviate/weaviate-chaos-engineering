7c81e4b34ac1aadb9a08275e44e151e99228ace7
Nate Wilkinson <nathan@weaviate.io>
2024-07-17 13:18:06 -0600

    hacky wip but working for basic results

--- corrupt_shards.sh
+++ corrupt_shards.sh
@@ -9,6 +9,7 @@ consistency_level=$4
 output_filepath=$5
 # SUCCESS, WAIT, ERR
 result="SUCCESS"
+got_trap="FALSE"
 runtime="NULL"
 
 start=`date +%s`
@@ -17,7 +18,8 @@ git submodule update --init --remote --recursive
 
 function wait_weaviate() {
   echo "Wait for Weaviate to be ready"
-  for _ in {1..15}; do
+  # TODO update to 60?
+  for _ in {1..60}; do
     if curl -sf -o /dev/null localhost:$1/v1/.well-known/ready; then
       echo "Weaviate is ready"
       return 0
@@ -26,9 +28,17 @@ function wait_weaviate() {
     echo "Weaviate is not ready on $1, trying again in 1s"
     sleep 1
   done
-  echo "ERROR: Weaviate is not ready in port ${1} after 10s"
+  echo "ERROR: Weaviate is not ready in port ${1} after 60s"
   result="WAIT"
+  log_result
   exit 2
+}
+
+function log_result() {
+  if [ "$result" != "WAIT" ] && [ "$got_trap" = "TRUE" ]; then
+    result="ERR"
+  fi
+  echo "${WEAVIATE_VERSION},${port_to_query},${node_to_corrupt},${corruptions_to_apply},${consistency_level},${DISABLE_RECOVERY_ON_PANIC},${result},${runtime}" >> "${output_filepath}"
 }
 
 function cleanup() {
@@ -40,12 +50,12 @@ function cleanup() {
 
 function shutdown() {
   # echo "Showing logs..."
-  # docker-compose -f apps/weaviate/docker-compose-replication.yml logs weaviate-node-1 weaviate-node-2 weaviate-node-3
+  docker-compose -f apps/weaviate/docker-compose-replication.yml logs weaviate-node-1 weaviate-node-2 weaviate-node-3
   cleanup
-  echo "${WEAVIATE_VERSION},${port_to_query},${node_to_corrupt},${corruptions_to_apply},${consistency_level},${DISABLE_RECOVERY_ON_PANIC},${result},${runtime}" >> "${output_filepath}"
+  log_result
 }
 cleanup || true
-trap 'result=ERR; shutdown; exit 1' SIGINT ERR
+trap 'got_trap="TRUE"; shutdown; exit 1' SIGINT ERR
 
 echo "Starting Weaviate..."
 docker compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-1 weaviate-node-2 weaviate-node-3
@@ -59,15 +69,40 @@ echo "Building all required containers"
 echo "Run setup"
 docker run --rm --network host --name corrupt-shards-setup -t corrupt-shards setup "$port_to_query" "$consistency_level"
 
+function overwrite() {
+  find "$1" \
+      -name "$2" \
+      -exec echo "overwriting {}" \; \
+      -exec bash -c 'echo "foo" > "$1"' _ {} \;
+}
+
+function byte0() {
+  find "$1" \
+      -name "$2" \
+      -exec echo "corrupting byte0 of {}" \; \
+      -exec bash -c 'printf "X" | dd conv=notrunc seek=0 bs=1 count=1 of="$1"' _ {} \;
+}
+
+function truncate1() {
+  find "$1" \
+      -name "$2" \
+      -exec echo "truncating {} to len 1" \; \
+      -exec truncate -s 1 "{}" \;
+}
+
+# function incrfilename() {
+#   find "$1" \
+#       -name "$2" \
+#       -exec echo "incrementing filename of {}" \; \
+#       -exec mv "{}" `expr {} + 42` \; \
+# }
+
 # TODO truncate stuff too, and change names
-if [[ $corruptions_to_apply =~ "wal" ]]; then
+if [[ $corruptions_to_apply =~ "wal_overwrite" ]]; then
   echo "Corrupting wal (and killing node)"
   # simulate crash so that *.wal files are not cleaned up
   docker kill "weaviate-weaviate-node-${node_to_corrupt}-1"
-  find "apps/weaviate/data-node-${node_to_corrupt}/pizza" \
-      -name 'segment-*.wal' \
-      -exec echo "truncating {}" \; \
-      -exec bash -c 'echo "natee" > "$1"' _ {} \;
+  overwrite "apps/weaviate/data-node-${node_to_corrupt}/pizza" 'segment-*.wal'
 fi
 docker compose -f apps/weaviate/docker-compose-replication.yml down
 
@@ -79,41 +114,56 @@ fi
 if [[ $corruptions_to_apply =~ "hnsw_condensed_byte0" ]]; then
   echo "Corrupting byte0 of hnsw condensed files"
   # corrupt hnsw index by replacing the first byte, seems like it needs to be a pathological corruption?
-  find "apps/weaviate/data-node-${node_to_corrupt}/pizza" \
-      -name '*.condensed' \
-      -exec echo "truncating {}" \; \
-      -exec bash -c 'printf "X" | dd conv=notrunc seek=0 bs=1 count=1 of="$1"' _ {} \;
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}/pizza" '*.condensed'
+fi
+if [[ $corruptions_to_apply =~ "classifications_byte0" ]]; then
+  echo "corrupt byte0 of classifications db"
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}" 'classifications.db'
+fi
+if [[ $corruptions_to_apply =~ "modules_byte0" ]]; then
+  echo "corrupt byte0 of modules db"
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}" 'modules.db'
+fi
+if [[ $corruptions_to_apply =~ "indexcount_byte0" ]]; then
+  echo "corrupt byte0 of indexcount file"
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}" 'indexcount'
+fi
+if [[ $corruptions_to_apply =~ "segment_bloom_byte0" ]]; then
+  echo "corrupt byte0 of boom files"
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}" 'segment-*.bloom'
+fi
+if [[ $corruptions_to_apply =~ "segment_cna_byte0" ]]; then
+  echo "corrupt byte0 of cna files"
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}" 'segment-*.cna'
 fi
 if [[ $corruptions_to_apply =~ "segment_db_byte0" ]]; then
   echo "Corrupting byte0 of segment db files"
-  find "apps/weaviate/data-node-${node_to_corrupt}/pizza" \
-      -name 'segment-*.db' \
-      -exec echo "truncating {}" \; \
-      -exec bash -c 'printf "X" | dd conv=notrunc seek=0 bs=1 count=1 of="$1"' _ {} \;
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}/pizza" 'segment-*.db'
+fi
+if [[ $corruptions_to_apply =~ "segment_wal_byte0" ]]; then
+  echo "corrupt byte0 of segment wal files"
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}" 'segment-*.wal'
 fi
 if [[ $corruptions_to_apply =~ "proplen_byte0" ]]; then
-  echo "Corrupting byte0 of proplengths files"
-  find "apps/weaviate/data-node-${node_to_corrupt}/pizza" \
-      -name 'proplengths' \
-      -exec echo "truncating {}" \; \
-      -exec bash -c 'printf "X" | dd conv=notrunc seek=0 bs=1 count=1 of="$1"' _ {} \;
+  echo "Corrupting byte0 of proplengths file"
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}/pizza" 'proplengths'
 fi
-# TODO other files/types of stuff/etc
-if [[ $corruptions_to_apply =~ "raft_db_byte0" ]]; then
-  echo "Corrupting byte0 of raft db files"
-  find "apps/weaviate/data-node-${node_to_corrupt}" \
-      -name 'raft.db' \
-      -exec echo "truncating {}" \; \
-      -exec bash -c 'xxd "$1" | head -n 1' _ {} \; \
-      -exec truncate -s 1 "{}" \; \
-      -exec bash -c 'xxd "$1" | head -n 1' _ {} \;
+if [[ $corruptions_to_apply =~ "version_byte0" ]]; then
+  echo "Corrupting byte0 of version file"
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}/pizza" 'version'
+fi
+if [[ $corruptions_to_apply =~ "schema_byte0" ]]; then
+  echo "Corrupting byte0 of schema db"
+  byte0 "apps/weaviate/data-node-${node_to_corrupt}/pizza" 'schema.db'
+fi
+# TODO files which dont have effect with byte0, try diff corruption?
+
+
+if [[ $corruptions_to_apply =~ "raft_db_truncate1" ]]; then
+  echo "truncate to len 1 for raft db files"
+  truncate1 "apps/weaviate/data-node-${node_to_corrupt}" 'raft.db'
 fi
 
-# TODO change name of empty commitlog file (inc/dec by 42)?
-# find apps/weaviate/data-node-1/pizza/*/main.hnsw.commitlog.d \
-#     -type f \
-#     -exec echo "moving {}" \; \
-#     -exec mv "{}" "{}.foo" \;
 # TODO do this in follow up test
 # hack: added "|| true" because moving the shard dir via find/exec returns a non-zero exit code
 # find apps/weaviate/data-node-1/pizza \
@@ -121,6 +171,8 @@ fi
 #     -d 1 \
 #     -exec echo "moving {}" \; \
 #     -exec mv "{}" "{}.foo" \; || true
+
+# TODO https://www.notion.so/weaviate/What-do-files-in-Weaviate-s-data-folder-do-6ac41928e85a437cb5784b0fe40f1864
 docker compose -f apps/weaviate/docker-compose-replication.yml up -d weaviate-node-1 weaviate-node-2 weaviate-node-3
 wait_weaviate 8080
 wait_weaviate 8081
--- wrapper_corrupt_shards.py
+++ wrapper_corrupt_shards.py
@@ -4,18 +4,48 @@ import math
 import subprocess
 import time
 
-weaviate_versions = ["1.25.4", "1.26.0-rc.0", "preview--4572237"]
+weaviate_versions = [
+    # "1.25.4",
+    # "1.26.0-rc.0",
+    # # "preview--4572237",
+    # # "preview--c543408",
+    # "preview--1c310dc",
+    # TODO main...
+    # 'local-nate-c181a94a6-v2',
+]
 disable_recovery_on_panics = [True] #, False]
-port_to_querys = [8080, 8081] #, 8082]
-node_to_corrupts = [1] #, 2, 3]
-corruptions = ['NULL', 'wal', 'hnsw_condensed_byte0', 'segment_db_byte0', 'proplen_byte0', 'raft_db_byte0']
+port_to_querys = [
+    # 8089,
+    8088,
+] #, 8087]
+node_ids_to_corrupts = [1] #, 2, 3]
+corruptions = [
+    # 'NULL', 
+    'hnsw_condensed_byte0', 
+    # 'classifications_byte0',
+    # 'modules_byte0',
+    # 'indexcount_byte0',
+    # 'segment_bloom_byte0',
+    # 'segment_cna_byte0',
+    # 'segment_db_byte0', 
+    # 'segment_wal_byte0',
+    # 'proplen_byte0', 
+    # 'version_byte0',
+    # 'schema_byte0',
+    # 'raft_db_truncate1',
+    # 'wal_overwrite', 
+]
 num_concurrent_corruptions = [1]
 num_corruption_combos = sum([math.comb(len(corruptions), i) for i in num_concurrent_corruptions])
-consistency_levels = ["ALL", "QUORUM"] #, "ONE"]
-num_repeats = 3
+consistency_levels = [
+    "QUORUM",
+    # "ALL", 
+    # "ONE",
+]
+num_repeats = 1
 output_filepath = f'corrupt_results/{int(time.time())}'
 
-estimated_num_runs = len(weaviate_versions) * len(disable_recovery_on_panics) * len(port_to_querys) * len(node_to_corrupts) * num_corruption_combos * len(consistency_levels) * num_repeats
+estimated_num_runs = len(weaviate_versions) * len(disable_recovery_on_panics) * len(port_to_querys) * len(node_ids_to_corrupts) * num_corruption_combos * len(consistency_levels) * num_repeats
 print('estimated_num_runs', estimated_num_runs)
 
 so_far_num_runs = 0
@@ -23,7 +53,7 @@ progress_cadence = 10
 for disable_recovery_on_panic in disable_recovery_on_panics:
     for consistency_level in consistency_levels:
         for port_to_query in port_to_querys:
-            for node_to_corrupt in node_to_corrupts:
+            for node_to_corrupt in node_ids_to_corrupts:
                 for num_corruption_to_apply in num_concurrent_corruptions:
                     for corruptions_to_apply in itertools.combinations(corruptions, num_corruption_to_apply):
                         for weaviate_version in weaviate_versions:
