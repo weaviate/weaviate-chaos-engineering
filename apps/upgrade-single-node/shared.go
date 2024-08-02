0b9a9f493fd950725663be40e81fb5f0775a506e
moogacs <contact@m-mooga.com>
2024-08-02 13:43:12 +0200

    add data integrity to single node upgrade test

---
+++ apps/upgrade-single-node/shared.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"os"
+
+	client "github.com/weaviate/weaviate-go-client/v4/weaviate"
+	"github.com/weaviate/weaviate/entities/models"
+)
+
+var (
+	numObjects = 10000
+	newObjects = 10
+	batchSize  = 5
+
+	node1Client = client.New(client.Config{Scheme: "http", Host: "localhost:8080"})
+
+	class = models.Class{
+		Class: "TunableConsistency",
+		Properties: []*models.Property{
+			{
+				Name:     "name",
+				DataType: []string{"string"},
+			},
+			{
+				Name:     "index",
+				DataType: []string{"int"},
+			},
+		},
+		ReplicationConfig: &models.ReplicationConfig{
+			Factor:       1,
+			AsyncEnabled: true,
+		},
+	}
+)
+
+func readObjectsFile(filename string) []*models.Object {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("failed to read objects file: %v", err)
+	}
+
+	var objects []*models.Object
+	err = json.Unmarshal(b, &objects)
+	if err != nil {
+		log.Fatalf("failed to unmarshal objects file: %v", err)
+	}
+
+	return objects
+}
+
+func randClient() *client.Client {
+	clients := []*client.Client{
+		node1Client,
+	}
+
+	return clients[rand.Intn(len(clients))]
+}
+
+func allClients() []*client.Client {
+	return []*client.Client{
+		node1Client,
+	}
+}
+
+func allClientsReversed() []*client.Client {
+	return []*client.Client{
+		node1Client,
+	}
+}
+
+func checkBatchInsertResult(created []models.ObjectsGetResponse, err error) {
+	if err != nil {
+		log.Fatalf("batch insert failed: %v", err)
+	}
+
+	for _, c := range created {
+		if c.Result != nil {
+			if c.Result.Errors != nil && c.Result.Errors.Error != nil {
+				log.Fatalf("failed to create obj: %+v, with status: %v",
+					c.Result.Errors.Error[0], c.Result.Status)
+			}
+		}
+	}
+}
--- upgrade_single_node_journey.sh
+++ upgrade_single_node_journey.sh
@@ -17,27 +17,89 @@ function wait_weaviate() {
   exit 1
 }
 
-echo "Building all required containers"
-function shutdown() {
+function shutdown() {  
   echo "Cleaning up ressources..."
   docker compose -f apps/weaviate/docker-compose-single-voter-without-node-name.yml down --remove-orphans
   rm -rf apps/weaviate/data* || true  
+  docker container rm -f generator &>/dev/null && echo 'Deleted container generator'
+  docker container rm -f importer &>/dev/null && echo 'Deleted container importer'
+  docker container rm -f importer_additional &>/dev/null && echo 'Deleted container importer_additional'
+  docker container rm -f cluster_read_repair &>/dev/null && echo 'Deleted container cluster_read_repair'
+  docker container rm -f cluster_healthy &>/dev/null && echo 'Deleted container cluster_healthy'
+  rm -rf workdir
 }
 trap 'shutdown; exit 1' SIGINT ERR
 
 
+function restart() {
+  echo "Restarting node ..."
+  docker-compose -f apps/weaviate/docker-compose-single-voter-without-node-name.yml kill weaviate-node-1
+  docker compose -f apps/weaviate/docker-compose-single-voter-without-node-name.yml up -d --force-recreate  weaviate-node-1
+  wait_weaviate 8080
+}
+
+function validateObjects() {
+  echo "Validate objects ..."
+  ( docker container rm -f cluster_healthy &>/dev/null )
+  ( cd apps/upgrade-single-node/ && docker build --build-arg APP_NAME=cluster_healthy -t cluster_healthy . || true )
+  if docker run --network host -v "$PWD/workdir/:/workdir/data" --name cluster_healthy -t cluster_healthy; then
+    echo "All objects read with consistency level ONE".
+  else
+    docker-compose -f apps/weaviate/docker-compose-single-voter-without-node-name.yml logs weaviate-node-1 
+    exit 1
+  fi
+}
+
+echo "Building all required containers"
+( cd apps/upgrade-single-node/ && docker build --build-arg APP_NAME=generator -t generator . )
+( cd apps/upgrade-single-node/ && docker build --build-arg APP_NAME=importer -t importer . )
+( cd apps/upgrade-single-node/ && docker build --build-arg APP_NAME=importer_additional -t importer_additional . )
+
+rm -rf apps/weaviate/data* || true  
+rm -rf workdir
+mkdir workdir
+touch workdir/data.json
+
+docker run --network host -v "$PWD/workdir/data.json:/workdir/data.json" --name generator -t generator
+
+echo "Done generating."
+
+echo "Starting Weaviate 1.25..."
 export WEAVIATE_NODE_VERSION=1.25.0
-
-
-echo "Starting Weaviate..."
 docker compose -f apps/weaviate/docker-compose-single-voter-without-node-name.yml up -d weaviate-node-1
 wait_weaviate 8080
 
-export WEAVIATE_NODE_VERSION=$WEAVIATE_VERSION
+# POST objects with consistency level ALL
+docker run --network host -v "$PWD/workdir/data.json:/workdir/data.json" --name importer -t importer
+
+# Read objects with consistency level ONE
+validateObjects
 
 echo "Upgrade Weaviate..."
+export WEAVIATE_NODE_VERSION=$WEAVIATE_VERSION
 docker compose -f apps/weaviate/docker-compose-single-voter-without-node-name.yml up -d --force-recreate  weaviate-node-1
 wait_weaviate 8080
+
+
+# Read objects with consistency level ONE
+validateObjects
+
+restart
+
+
+echo "Import additional objects"
+if docker run --network host -v "$PWD/workdir/data.json:/workdir/data.json" --name importer_additional -t importer_additional; then
+  echo "All objects added with consistency level QUORUM with one node down".
+else
+  docker-compose -f apps/weaviate/docker-compose-single-voter-without-node-name.yml logs weaviate-node-1
+  exit 1
+fi
+
+validateObjects
+
+restart
+validateObjects
+
 
 echo "Success!"
 shutdown
