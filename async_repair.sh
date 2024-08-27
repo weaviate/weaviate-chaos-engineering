2479e3f057e88cac4091f846d9992aa86dfcc5aa
Jose Luis Franco Arza <jose@weaviate.io>
2024-08-27 11:37:37 +0200

    Add delay to ensure async replication repairs the node

--- async_repair.sh
+++ async_repair.sh
@@ -50,6 +50,8 @@ fi
 echo "Restart node 3"
 docker compose -f apps/weaviate/docker-compose-replication_single_voter.yml up -d weaviate-node-3
 wait_weaviate 8082
+# Give some time for async repair to restore the objects in the restarted node
+sleep 5
 if docker run --network host -v "$PWD/workdir/:/workdir/data" --name cluster_async_repair -t cluster_async_repair; then
   echo "All objects read with consistency level ALL after weaviate-node-3 restarted".
 else
