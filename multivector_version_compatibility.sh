4a4c5b0e66d79da704ddcca1cd8105162c72c87d
Nate Wilkinson <nathan@weaviate.io>
2025-03-12 11:08:10 -0600

    remove error comments

--- multivector_version_compatibility.sh
+++ multivector_version_compatibility.sh
@@ -19,41 +19,6 @@ function wait_weaviate() {
 
 
 declare -a no_multivector_versions=(
-# Traceback (most recent call last):
-#   File "/workdir/verify_multivectors.py", line 43, in <module>
-#     assert len(normal_objects.objects) == 1, f"Expected 1 object in {NORMAL_VECTOR_COLLECTION_NAME}, got {len(normal_objects.objects)}"
-#            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# AssertionError: Expected 1 object in Normalvector, got 0
-  # "1.24.26"
-
-# Traceback (most recent call last):
-#   File "/workdir/verify_multivectors.py", line 71, in <module>
-#     normal_results = normal_collection.query.near_vector(
-#                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#   File "/usr/local/lib/python3.11/site-packages/weaviate/syncify.py", line 23, in sync_method
-#     return _EventLoopSingleton.get_instance().run_until_complete(
-#            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-#   File "/usr/local/lib/python3.11/site-packages/weaviate/event_loop.py", line 42, in run_until_complete
-#     return fut.result()
-#            ^^^^^^^^^^^^
-#   File "/usr/local/lib/python3.11/concurrent/futures/_base.py", line 456, in result
-#     return self.__get_result()
-#            ^^^^^^^^^^^^^^^^^^^
-#   File "/usr/local/lib/python3.11/concurrent/futures/_base.py", line 401, in __get_result
-#     raise self._exception
-#   File "/usr/local/lib/python3.11/site-packages/weaviate/collections/queries/near_vector/query.py", line 92, in near_vector
-#     res = await self._query.near_vector(
-#                 ^^^^^^^^^^^^^^^^^^^^^^^^
-#   File "/usr/local/lib/python3.11/site-packages/weaviate/collections/grpc/query.py", line 248, in near_vector
-#     near_vector=self._parse_near_vector(
-#                 ^^^^^^^^^^^^^^^^^^^^^^^^
-#   File "/usr/local/lib/python3.11/site-packages/weaviate/collections/grpc/shared.py", line 376, in _parse_near_vector
-#     vector_per_target_tmp, near_vector_grpc = self._vector_per_target(
-#                                               ^^^^^^^^^^^^^^^^^^^^^^^^
-#   File "/usr/local/lib/python3.11/site-packages/weaviate/collections/grpc/shared.py", line 129, in _vector_per_target
-#     raise WeaviateInvalidInputError(
-# weaviate.exceptions.WeaviateInvalidInputError: Invalid input provided: The number of target vectors must be equal to the number of vectors..
-  # "1.25.29"
   "1.28.4"
 )
 
@@ -108,7 +73,7 @@ function test_version_sequence() {
     docker-compose -f apps/weaviate/docker-compose.yml up -d
     wait_weaviate
 
-    # verify queryable on multivectors after going through no multivector support
+    # verify queryable on multivectors after going through intermediate version
     docker run --network host -t multivector_version_compatibility python3 verify_multivectors.py
 
     # shutdown weaviate
