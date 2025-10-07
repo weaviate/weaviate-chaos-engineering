25445ce7ae75d9697cb82d079b651bc4b9093774
Tommy Smith <tommy@weaviate.io>
2025-10-07 10:26:49 +0100

    Fix naming in bash script

--- server_side_batching_with_rolling_restart.sh
+++ server_side_batching_with_rolling_restart.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo "Building all required containers"
-( cd apps/batch-import-shutdown-journey/ && docker build -t batch_import_shutdown_journey . )
+( cd apps/server-side-batching-with-rolling-restart/ && docker build -t server_side_batching_with_rolling_restart . )
 
-echo "Start the batch import journey"
-container_id=$(docker run -d --network host -t batch_import_shutdown_journey python3 run.py)
+echo "Start the journey"
+container_id=$(docker run -d --network host -t server_side_batching_with_rolling_restart python3 run.py)
 
 echo "Wait a bit to let the import start"
 shuf -i 1-10 -n 1 | xargs sleep # wait a bit before performing rolling restart
@@ -14,7 +14,7 @@ shuf -i 1-10 -n 1 | xargs sleep # wait a bit before performing rolling restart
 echo "Perform a rolling restart of the weaviate cluster"
 kubectl rollout restart statefulset/weaviate -n weaviate
 
-echo "Following the logs of the batch import journey"
+echo "Following the logs of the journey"
 docker logs -f "$container_id"
 
 exit_code=$(docker inspect "$container_id" --format='{{.State.ExitCode}}')
