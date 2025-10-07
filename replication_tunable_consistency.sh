12a0224dc9e8193d6407335f333a897768897f4f
moogacs <contact@m-mooga.com>
2025-10-07 11:34:03 +0200

    sleep between nodes joining to avoid races

--- replication_tunable_consistency.sh
+++ replication_tunable_consistency.sh
@@ -74,6 +74,8 @@ fi
 # Restart dead nodes, read objects with consistency level ALL
 docker compose -f $COMPOSE up -d weaviate-node-2
 wait_weaviate 8081
+# sleep to avoid any races in joining the cluster
+sleep 3
 docker compose -f $COMPOSE up -d weaviate-node-3
 wait_weaviate 8082
 if docker run --network host -v "$PWD/workdir/:/workdir/data" --name cluster_one_node_remaining -t cluster_one_node_remaining; then
