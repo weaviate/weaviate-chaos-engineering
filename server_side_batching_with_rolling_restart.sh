a26b8c950923e33804677ba927f16c8e5ae3b2b8
Tommy Smith <tommy@weaviate.io>
2026-02-20 15:28:51 +0000

    Improve logging of test

--- server_side_batching_with_rolling_restart.sh
+++ server_side_batching_with_rolling_restart.sh
@@ -11,12 +11,19 @@ container_id=$(docker run -d --network host -t server_side_batching_with_rolling
 echo "Wait a bit to let the import start"
 shuf -i 5-20 -n 1 | xargs sleep # wait a bit before performing rolling restart
 
+echo "Logs prior to restart"
 docker logs -t "$container_id"
 
 echo "Perform a rolling restart of the weaviate cluster"
 kubectl rollout restart sts/weaviate -n weaviate
 
-echo "Following the logs of the journey"
+echo "Logs following the restart"
+docker logs -t "$container_id"
+
+echo "Show the pods restarting"
+kubectl get pods -n weaviate
+
+echo "Follow the logs until the journey completes"
 docker logs -ft "$container_id"
 
 exit_code=$(docker inspect "$container_id" --format='{{.State.ExitCode}}')
