b6a27000a2d0d8de4d708e5db1ecf075e5036663
Tommy Smith <tommy@weaviate.io>
2026-02-20 15:27:25 +0000

    Print container logs before restarting then follow

--- server_side_batching_with_rolling_restart.sh
+++ server_side_batching_with_rolling_restart.sh
@@ -11,11 +11,13 @@ container_id=$(docker run -d --network host -t server_side_batching_with_rolling
 echo "Wait a bit to let the import start"
 shuf -i 5-20 -n 1 | xargs sleep # wait a bit before performing rolling restart
 
+docker logs -t "$container_id"
+
 echo "Perform a rolling restart of the weaviate cluster"
-kubectl rollout restart statefulset/weaviate -n weaviate
+kubectl rollout restart sts/weaviate -n weaviate
 
 echo "Following the logs of the journey"
-docker logs -f "$container_id"
+docker logs -ft "$container_id"
 
 exit_code=$(docker inspect "$container_id" --format='{{.State.ExitCode}}')
 echo "Container exited with code $exit_code"
